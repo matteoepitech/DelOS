65bab51f874d7eaeddec481f1debe58603ce5f58
matteoepitech <matteo.tutti@epitech.eu>
2025-10-11 20:55:16 +0200

    feat(boot): add protected mode

--- boot/boot.s
+++ boot/boot.s
@@ -1,87 +1,69 @@
 ; boot.s
-BITS 16
 ORG 0x7c00
 
-; BIOS put into dl the disk number. So we save it back.
-mov [BOOT_DISK], dl
+; ======================================================
+; REAL MODE
+; ======================================================
+[BITS 16]
 
-xor ax, ax
-mov ds, ax
-mov es, ax
-mov ss, ax
-mov sp, 0x7c00
-mov bp, sp
+CODE_SEG equ code_descriptor - GDT_Start ; = 0x08 which is 1 * 8, so the CPU know it's the index number 1 for the code segment
+DATA_SEG equ data_descriptor - GDT_Start ; = 0x10 which is 2 * 8, so the CPU know it's the index number 2 for the data segment
 
-mov si, msg_start
-call print_string
+mov [BOOT_DISK_ID], dl  ; BIOS set dl to the loaded disk ID, so we copy it
+cli                     ; disable material interupts
+lgdt [GDT_Descriptor]   ; load the global descriptor table into the register GDTR
+mov eax, cr0            ; copy the cr0 = control register 0
+or eax, 0b1             ; set the PE (protected enable) first bit to 1
+mov cr0, eax            ; set back the control register
 
-call enable_a20
+jmp dword CODE_SEG:start_protected_mode ; do a far jump to load the code segment
+jmp $                                   ; should never goes here
 
-mov bx, 0x7e00 ; where to store data 0x0000:0x7E00 = 0x7E00
-mov ah, 2 ; read storage from disk
-mov al, 1 ; sector to read count
-mov ch, 0 ; cylinder
-mov dh, 0 ; head
-mov cl, 2 ; sector
-mov dl, [BOOT_DISK] ; drive
-int 0x13
+; flat model of memory
+; data and code are in the same space and extended on the whole memory
+; the global descriptor table should be at the end of the real mode code
+GDT_Start:
+    null_descriptor:
+        dq 0x00
+    code_descriptor:
+        dw 0xffff
+        dw 0x00
+        db 0x00
+        db 0b10011010
+        db 0b11001111
+        db 0x00
+    data_descriptor:
+        dw 0xffff
+        dw 0x00
+        db 0x00
+        db 0b10010010
+        db 0b11001111
+        db 0x00
+GDT_End:
+GDT_Descriptor:
+    dw GDT_End - GDT_Start - 1  ; size
+    dd GDT_Start                ; start
 
-mov si, 0x7e00
-call print_string
+; ======================================================
+; PROTECTED MODE
+; ======================================================
+[BITS 32]
 
-jmp $
-
-; ----------------------------------------
-; void print_string(char *string)
-;
-; Parameters:
-;   - si = string address
-; ----------------------------------------
-print_string:
-    lodsb
-    test al, al
-    jz .done
-    mov ah, 0x0e
-    xor bh, bh
-    int 0x10
-    jmp print_string
-
-.done:
-    ret
-
-; ----------------------------------------
-; void enable_a20(void)
-; ----------------------------------------
-enable_a20:
-    mov ah, 0x24        ; "Enable A20" BIOS function
-    int 0x15
-    jnc .done
-    jmp .fail
-
-.done:
-    mov si, msg_a20_ok
-    call print_string
-    ret
-
-.fail:
-    mov si, msg_a20_ko
-    call print_string
-    ret
-
+start_protected_mode:
+    mov al, 'A'
+    mov ah, 0x0f
+    mov [0xb8000], ax
+    jmp $
 
 ; ======================================================
 ; DATA
 ; ======================================================
 msg_start db "Bootloader started...", 13, 10, 0
-msg_a20_ok db "A20 line enabled!", 13, 10, 0
-msg_a20_ko db "A20 line failed!", 13, 10, 0
 
-BOOT_DISK db 0
+BOOT_DISK_ID db 0
 
 ; ======================================================
 ; BOOT SIGNATURE
 ; ======================================================
 times 510-($-$$) db 0
 dw 0xaa55
-
-times 512 db 'A'
