0136dd0343c4c876dd0eafef637a28d23b11b73d
matteoepitech <matteo.tutti@epitech.eu>
2025-10-12 15:08:52 +0200

    feat(alphakernel): add the alphabet print in the kernel main

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -5,9 +5,13 @@
 ** Kernel main
 */
 
+#include <stdint.h>
+
 void kmain(void)
 {
-    *(char *) 0xb8000 = 'Z';
+    for (int i = 0; i < 26; i++) {
+        *(unsigned char *) ((uintptr_t) 0xb8000 + i * 2) = 'A' + i;
+    }
     while (1);
     return;
 }
