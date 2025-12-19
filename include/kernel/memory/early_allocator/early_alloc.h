ba1052f366cf6b7bdf027f28a891a1588e506c5c
matteoepitech <matteo.tutti@epitech.eu>
2025-12-19 10:37:52 +0100

    feat(memory): add a placeholder files for bump allocator (early allocator)

---
+++ include/kernel/memory/early_allocator/early_alloc.h
@@ -0,0 +1,15 @@
+/*
+** DELOS PROJECT, 2025
+** include/kernel/misc/early_alloc
+** File description:
+** early_alloc header file
+*/
+
+#include "types.h"
+
+#ifndef KERNEL_MEMORY_EARLY_ALLOCATOR_H_
+    #define KERNEL_MEMORY_EARLY_ALLOCATOR_H_
+
+
+
+#endif /* ifndef KERNEL_MEMORY_EARLY_ALLOCATOR_H_ */
---
+++ src/kernel/memory/early_allocator/early_alloc.c
@@ -0,0 +1,8 @@
+/*
+** DELOS PROJECT, 2025
+** src/kernel/memory/early_allocator/early_alloc
+** File description:
+** early allocator source file (bump allocator)
+*/
+
+#include "kernel/memory/early_allocator/early_alloc.h"
