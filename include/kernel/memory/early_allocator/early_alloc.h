2d7aa7f526407cad009c84ac91f7ad57072e4524
matteoepitech <matteo.tutti@epitech.eu>
2025-12-19 11:49:41 +0100

    feat(memory): add the linker script symbol for the start of the early heap

--- include/kernel/memory/early_allocator/early_alloc.h
+++ include/kernel/memory/early_allocator/early_alloc.h
@@ -10,6 +10,26 @@
 #ifndef KERNEL_MEMORY_EARLY_ALLOCATOR_H_
     #define KERNEL_MEMORY_EARLY_ALLOCATOR_H_
 
+/*
+ * @brief This variable comes from the linker script which place it next to the end of the code space.
+ *        Basically: .text -> .rodata -> .data -> .bss -> _kernel_start_early_heap pointer
+ *
+ *        INFO: This is an address which redirect to a byte.
+ */
+extern uint8_t _kernel_start_early_heap;
 
+/**
+ * @brief Basically a malloc() implementation for kernel purpose only in early stage.
+ *        WARN: Do not use this allocator.
+ *        INFO: This allocator is using a bump allocator / watermark algorithm.
+ *              It contains a reference to the current ptr and just add the bytes to move.
+ *              It not contains a free function since the size of the "heap" is "pre-used".
+ *
+ * @param bytes  The number of bytes to allocate
+ *
+ * @return The pointer to the size memory allocated.
+ */
+void *
+kearly_malloc(uint32_t bytes);
 
 #endif /* ifndef KERNEL_MEMORY_EARLY_ALLOCATOR_H_ */
--- src/kernel/memory/early_allocator/early_alloc.c
+++ src/kernel/memory/early_allocator/early_alloc.c
@@ -6,3 +6,26 @@
 */
 
 #include <kernel/memory/early_allocator/early_alloc.h>
+#include <utils/misc/print.h>
+#include <defines.h>
+
+/**
+ * @brief Basically a malloc() implementation for kernel purpose only in early stage.
+ *        WARN: Do not use this allocator.
+ *        INFO: This allocator is using a bump allocator / watermark algorithm.
+ *              It contains a reference to the current ptr and just add the bytes to move.
+ *              It not contains a free function since the size of the "heap" is "pre-used".
+ *
+ * @param bytes  The number of bytes to allocate
+ *
+ * @return The pointer to the size memory allocated.
+ */
+void *
+kearly_malloc(uint32_t bytes)
+{
+    if (bytes <= 0) {
+        return NULL;
+    }
+    KPRINTF_DEBUG("%p", _kernel_start_early_heap);
+    return NULL;
+}
