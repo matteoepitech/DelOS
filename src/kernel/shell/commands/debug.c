d666641a5792dd4610096c15e91725abf298092a
matteoepitech <matteo.tutti@epitech.eu>
2025-12-31 21:33:41 +0100

    chore(debug): change the debug command, everything is working fine

--- src/kernel/shell/commands/debug.c
+++ src/kernel/shell/commands/debug.c
@@ -28,19 +28,10 @@
 uint8_t
 kshell_debug(UNUSED uint32_t argc, UNUSED char *argv[])
 {
-    vfs_node_t *file = NULL;
-
-    kvfs_create(kvfs_root_mount_dir, "file.txt");
     kvfs_mkdir(kvfs_root_mount_dir, "secret");
     vfs_node_t *secret_dir = kvfs_lookup(kvfs_root_mount_dir, "secret");
     kvfs_create(secret_dir, "secret.file");
-    file = kvfs_open("/file.txt");
-    if (file == NULL) {
-        KPRINTF_ERROR("debug: cannot opening file.txt");
-        return OK_TRUE;
-    }
-    kvfs_close(file);
-    file = kvfs_open("/secret/secret.file");
+    vfs_node_t *file = kvfs_open("/secret/secret.file");
     if (file == NULL) {
         KPRINTF_ERROR("debug: cannot opening secret.file");
         return OK_TRUE;
@@ -48,10 +39,16 @@ kshell_debug(UNUSED uint32_t argc, UNUSED char *argv[])
     kvfs_close(file);
     kvfs_close(secret_dir);
 
-    vfs_node_t *a = kvfs_open("/secret/secret.file");
     char buffer[] = "qdkjqx829N";
+    vfs_node_t *a = kvfs_open("/secret/secret.file");
     kvfs_write(a, buffer, sizeof(buffer));
     kvfs_close(a);
+
+    char content[1024] = {0};
+    vfs_node_t *b = kvfs_open("/secret/secret.file");
+    kvfs_read(b, content, sizeof(content));
+    kvfs_close(b);
+    KPRINTF_OK("%s", content);
 
     return KO_FALSE;
 }
